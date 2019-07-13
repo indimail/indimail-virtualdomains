5e7bdf055863bd1883e53b7fded6baaee789335f
Manvendra Bhangui <mbhangui@gmail.com>
2019-07-13 23:05:07 +0530

    moved to indimail-mta repo

--- ucspi-tcp-0.88/date@.sh
+++
@@ -1,6 +0,0 @@
-# $Log: date@.sh,v $
-# Revision 1.1  2003-12-31 19:57:58+05:30  Cprogrammer
-# Initial revision
-#
-#
-HOME/bin/tcpclient -RHl0 -- "${1-0}" 13 sh -c 'exec HOME/bin/delcr <&6' | cat -v
