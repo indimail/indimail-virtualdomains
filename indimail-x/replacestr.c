32b23e471dcc3997c881084814f8e43f1ae45bf7
Manvendra Bhangui <mbhangui@gmail.com>
2020-07-23 11:20:04 +0530

    added comments for complicated code

--- indimail-x/replacestr.c
+++ indimail-x/replacestr.c
@@ -1,5 +1,8 @@
 /*
  * $Log: replacestr.c,v $
+ * Revision 1.3  2020-07-23 11:19:26+05:30  Cprogrammer
+ * added comments as code is complicated
+ *
  * Revision 1.2  2020-07-09 13:42:50+05:30  Cprogrammer
  * BUGFIX: initialize stralloc variable buf
  *
@@ -17,10 +20,10 @@
 #endif
 
 #ifndef	lint
-static char     sccsid[] = "$Id: replacestr.c,v 1.2 2020-07-09 13:42:50+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsid[] = "$Id: replacestr.c,v 1.3 2020-07-23 11:19:26+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
-/*
+/*-
  * returns 0 if search string ch not found
  * and leaves buf untouched
  */
@@ -35,6 +38,7 @@ replacestr(char *str, char *ch, char *rch, stralloc *buf)
 	rchlen = str_len(rch);
 	buf->len = 0;
 	for (ptr = str, prev = cnt = 0; *ptr;) {
+		/*- no more characters found from this point onward */
 		if (!(s = str_str(ptr, ch))) {
 			if (cnt) {
 				/*- copy remaining data */
@@ -47,11 +51,13 @@ replacestr(char *str, char *ch, char *rch, stralloc *buf)
 			return (cnt);
 		}
 		i = s - str;
+		/*- copy data upto the found character */
 		if (!stralloc_catb(buf, ptr, i - prev))
 			return (-1);
+		/*- replace the found character with replacement */
 		if (!stralloc_catb(buf, rch, rchlen))
 			return (-1);
-		ptr = str + i + chlen;
+		ptr = str + i + chlen; /*- move past the found character */
 		prev = i + chlen;
 		cnt++;
 	}
