2efb618264e44607bc55498f431b33863385da4e
Manvendra Bhangui <mbhangui@gmail.com>
2020-09-21 18:48:41 +0530

    FreeBSD port

--- nssd-x/check_getpw.c
+++ nssd-x/check_getpw.c
@@ -1,5 +1,8 @@
 /*
  * $Log: check_getpw.c,v $
+ * Revision 1.4  2020-09-21 18:48:24+05:30  Cprogrammer
+ * FreeBSD port
+ *
  * Revision 1.3  2019-06-13 21:22:20+05:30  Cprogrammer
  * removed getversion_check_getpw_c function
  *
@@ -10,44 +13,56 @@
  * Initial revision
  *
  */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <pwd.h>
+#ifdef HAVE_SHADOW_H
 #include <shadow.h>
+#endif
 
 #ifndef	lint
-static char     rcsid[] = "$Id: check_getpw.c,v 1.3 2019-06-13 21:22:20+05:30 Cprogrammer Exp mbhangui $";
+static char     rcsid[] = "$Id: check_getpw.c,v 1.4 2020-09-21 18:48:24+05:30 Cprogrammer Exp mbhangui $";
 #endif
 
 int
 main(int argc, char **argv)
 {
 	struct passwd *pw;
+#ifdef HAVE_SHADOW
 	struct spwd   *spw;
+#endif
 	int i;
 
-	if (!getuid())
-	{
+	if (!getuid()) {
 		fprintf(stderr, "you must not be root!!\n");
 		return (1);
 	}
-	for (i = 1;i < argc;i++)
-	{
-		if (!(pw = getpwnam(argv[i])))
-		{
+	for (i = 1;i < argc;i++) {
+		if (!(pw = getpwnam(argv[i]))) {
 			fprintf(stderr, "%s: No such user\n", argv[i]);
 			return(1);
 		}
 		printf("%s:", pw->pw_name);
-		if (!(spw = getspnam(argv[i])))
-		{
+#ifdef HAVE_SHADOW
+		if (!(spw = getspnam(argv[i]))) {
 			fprintf(stderr, "getspnam: %s: %s\n", argv[i], errno ? strerror(errno) : "not found in shadow");
 			printf("%s:", pw->pw_passwd);
 		} else
 			printf("%s:", spw->sp_pwdp);
+#else
+		printf("%s:", pw->pw_passwd);
+#endif
+#if defined(__FreeBSD__)
+		printf("%d:%d:%ld:%s:%s:%s:%s:%ld\n", pw->pw_uid, pw->pw_gid, pw->pw_change, pw->pw_class, 
+			pw->pw_gecos, pw->pw_dir, pw->pw_shell, pw->pw_expire);
+#else
 		printf("%d:%d:%s:%s:%s\n", pw->pw_uid, pw->pw_gid, pw->pw_gecos, pw->pw_dir, pw->pw_shell);
+#endif
 	}
 	return(0);
 }
