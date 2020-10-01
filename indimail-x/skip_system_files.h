dda4a459d3e730aea3bcaac75a6b48ef2bf85ae0
Manvendra Bhangui <mbhangui@gmail.com>
2020-10-01 18:29:14 +0530

    fixed typo in header guard

--- indimail-x/skip_system_files.h
+++ indimail-x/skip_system_files.h
@@ -1,11 +1,14 @@
 /*
  * $Log: skip_system_files.h,v $
+ * Revision 1.2  2020-10-01 18:28:45+05:30  Cprogrammer
+ * fixed typo
+ *
  * Revision 1.1  2019-04-13 23:39:27+05:30  Cprogrammer
  * skip_system_files.h
  *
  */
 #ifndef SKIP_SYSTEM_FILES
-#define SKYP_SYSTEM_FILES
+#define SKIP_SYSTEM_FILES
 
 int             skip_system_files(char *);
 
