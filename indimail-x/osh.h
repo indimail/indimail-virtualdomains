d54e03c56ced571c5f1a93ce0ae332d84e6cb4a5
Manvendra Bhangui <mbhangui@gmail.com>
2021-09-01 18:31:20 +0530

    updated function prototypes

--- indimail-x/osh.h
+++ indimail-x/osh.h
@@ -1,5 +1,8 @@
 /*
  * $Log: osh.h,v $
+ * Revision 1.2  2021-09-01 18:29:59+05:30  Cprogrammer
+ * updated function prototyps
+ *
  * Revision 1.1  2019-04-13 23:39:27+05:30  Cprogrammer
  * osh.h
  *
