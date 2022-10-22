849db4fffae2b20dcd6017fdf6fe0da45b4f5a25
Manvendra Bhangui <mbhangui@gmail.com>
2022-10-22 12:56:19 +0530

    use date822fmt() for local time

--- indimail-x/get_localtime.c
+++ indimail-x/get_localtime.c
@@ -1,5 +1,8 @@
 /*
  * $Log: get_localtime.c,v $
+ * Revision 1.3  2022-10-22 12:53:49+05:30  Cprogrammer
+ * use date822fmt() for local time
+ *
  * Revision 1.2  2020-09-17 14:47:36+05:30  Cprogrammer
  * FreeBSD fix
  *
@@ -13,58 +16,30 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+/*
 #ifdef HAVE_TIME_H
 #include <time.h>
 #endif
+*/
 #ifdef HAVE_QMAIL
-#include <stralloc.h>
-#include <strerr.h>
-#include <fmt.h>
+#include <datetime.h>
+#include <date822fmt.h>
+#include <now.h>
 #endif
 
 #ifndef	lint
-static char     sccsid[] = "$Id: get_localtime.c,v 1.2 2020-09-17 14:47:36+05:30 Cprogrammer Exp mbhangui $";
+static char     sccsid[] = "$Id: get_localtime.c,v 1.3 2022-10-22 12:53:49+05:30 Cprogrammer Exp mbhangui $";
 #endif
-
-static void
-die_nomem()
-{
-	strerr_warn1("get_localtime: out of memory", 0);
-	_exit(111);
-}
 
 char           *
 get_localtime()
 {
-	time_t          tmval;
-	struct tm      *tmptr;
-	static stralloc tmpbuf = {0};
-#ifdef USE_TIMEZONE
-	int             mins, hours, len;
-#endif
+	struct datetime dt;
+	int             len;
+	static char     buf[DATE822FMT];
 
-	tmval = time(0);
-	tmptr = localtime(&tmval);
-	if (!stralloc_copys(&tmpbuf, asctime(tmptr)))
-		die_nomem();
-	if (tmpbuf.s[tmpbuf.len - 1] == '\n')
-		tmpbuf.len--; /*- remove newline */
-#ifdef USE_TIMEZONE
-	mins = (timezone % 3600) / 60;
-	if (mins < 0)
-		mins = -mins;
-	hours = timezone / 3600;
-	if (!stralloc_cats(&tmpbuf, tzname[0]) ||
-			!stralloc_append(&tmpbuf, " ") ||
-			!stralloc_catb(&tmpbuf, strnum, fmt_uint(strnum, hours)) ||
-			!stralloc_append(&tmpbuf, ":"))
-		die_nomem();
-	if (mins < 10 && !stralloc_append(&tmpbuf, "0"))
-		die_nomem();
-	if (!stralloc_catb(&tmpbuf, strnum, fmt_uint(strnum, mins)))
-		die_nomem();
-#endif
-	if (!stralloc_0(&tmpbuf))
-		die_nomem();
-	return (tmpbuf.s);
+	datetime_tai(&dt, now());
+	len = date822fmt(buf, &dt);
+	buf[len - 1] = 0;
+	return buf;
 }
